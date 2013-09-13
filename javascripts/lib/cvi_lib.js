ddf5e26c8166b75d582bcba81a26a8dbe9042467
SprayHank <fantasyoui@gmail.com>
2013-09-13 19:49:14 +0800

    保存

--- javascripts/lib/cvi_lib.js
+++ javascripts/lib/cvi_lib.js
@@ -1,8 +1,32 @@
 ;
-var cvi_lib;
+var CVI;
 (function() {
     "use strict";
-    cvi_lib = {
-        vml: !-[1, ] && document.all && document.namespaces
+    CVI = function() {};
+    CVI.prototype = {
+        vml: function supportsVml() {
+            if(typeof supportsVml.supported == "undefined") {
+                var a = document.documentElement.appendChild(document.createElement('div'));
+                a.innerHTML = '<v:shape id="vml_flag1" adj="1" />';
+                var b = a.firstChild;
+                b.style.behavior = "url(#default#VML)";
+                supportsVml.supported = b ? typeof b.adj == "object" : true;
+                a.parentNode.removeChild(a);
+            }
+            return supportsVml.supported
+        }(),
+        G: function(v) {return(document.getElementById(v));},
+        R: function(v) {return(document.body.removeChild(v));},
+        A: function(v) {return(document.body.appendChild(v));},
+        C: function(v) {return(document.createElement(v));},
+        extend: function(subClass, superClass) {
+            //见到网上有人是这样写的
+            //subClass.prototype=superClass.prototype;
+            //subClass.prototype.constructor=subClass;
+            var F = function() {};
+            F.prototype = superClass.prototype;
+            subClass.prototype = new F();
+            subClass.prototype.constructor = subClass;
+        }
     };
 })();
