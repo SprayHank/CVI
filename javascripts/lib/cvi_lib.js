a7b47a9a0ba0173afbc26cf8fdf50f753508e70d
SprayHank <fantasyoui@gmail.com>
2013-09-14 16:35:09 +0800

    调试函数和类型字符串

--- javascripts/lib/cvi_lib.js
+++ javascripts/lib/cvi_lib.js
@@ -19,14 +19,33 @@ var CVI;
         //Canvas Support detection
         cvs: (function isCanvasSupported(){
             var elem = document.createElement('CANVAS');
-            return !!(elem.getContext && elem.getContext('2D'));
+            return !!(elem.getContext && elem.getContext('2d'));
+        })(),
+        //type
+        type: (function(){
+            if((function supportsVml() {
+                if(typeof supportsVml.supported == "undefined") {
+                    var a = document.documentElement.appendChild(document.createElement('div'));
+                    a.innerHTML = '<v:shape id="vml_flag1" adj="1" />';
+                    var b = a.firstChild;
+                    b.style.behavior = "url(#default#VML)";
+                    supportsVml.supported = b ? typeof b.adj == "object" : true;
+                    a.parentNode.removeChild(a);
+                }
+                return supportsVml.supported
+            })())return 'VML';
+            if((function isCanvasSupported(){
+                var elem = document.createElement('CANVAS');
+                return !!(elem.getContext && elem.getContext('2d'));
+            })())return 'CVS';
+            return ''
         })(),
         //Format the Color to #XXXXXX from #XXX even if not color , turn to #000000
         Color: function(v) {
-            if(v.match(/^#[0-9a-f]{6}$/i)) {return v;}
+            if(v.match(/^#[0-9a-f]{6}$/i)) {return v}
             else if(v.match(/^#[0-9a-f]{3}$/i)) {
-                return '#' + v.substr(1, 1) + v.substr(1, 1) + v.substr(2, 1) + v.substr(2, 1) + v.substr(3, 1) + v.substr(3, 1);
-            } else {return '#000000';}
+                return '#' + v.substr(1, 1) + v.substr(1, 1) + v.substr(2, 1) + v.substr(2, 1) + v.substr(3, 1) + v.substr(3, 1)
+            } else {return '#000000'}
         },
         //get the Color from hex to rgb ,return like '35, 98, 106'
         getRGB: function(v) {
@@ -44,6 +63,24 @@ var CVI;
         A: function(v) {return(document.body.appendChild(v));},
         //create a element
         C: function(v) {return(document.createElement(v));},
+        //debug function
+        debug: function(s, v){
+            if(this.verbose){
+                s = s.toUpperCase() || 'LOG';
+                if(window.console) {
+                    if(!window.console.warn) {
+                        window.console.log(s + ': ' + v);
+                    } else {
+                        window.console[s.toLowerCase() || 'log'](v);
+                    }
+                } else if(window.opera) {
+                    opera.postError(s + ': ' + v);
+                } else {
+                    window.document.title = s + ': ' + v;
+                }
+            }
+            return false
+        },
         //extend an object like jQuery but no deep
         extend: function() {
             var destination = arguments[0] || {},
