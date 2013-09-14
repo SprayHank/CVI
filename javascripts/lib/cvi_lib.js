dfcfe2f43f843ec8e99d7fef90316c9850530390
SprayHank <fantasyoui@gmail.com>
2013-09-14 11:19:56 +0800

    CVI类

--- javascripts/lib/cvi_lib.js
+++ javascripts/lib/cvi_lib.js
@@ -4,7 +4,7 @@ var CVI;
     "use strict";
     CVI = function() {};
     CVI.prototype = {
-        vml: function supportsVml() {
+        vml: (function supportsVml() {
             if(typeof supportsVml.supported == "undefined") {
                 var a = document.documentElement.appendChild(document.createElement('div'));
                 a.innerHTML = '<v:shape id="vml_flag1" adj="1" />';
@@ -14,12 +14,40 @@ var CVI;
                 a.parentNode.removeChild(a);
             }
             return supportsVml.supported
-        }(),
+        })(),
+        cvs: !this.vml,
+        getRGB: function(v) {
+            function hex2dec(h) {return(Math.max(0, Math.min(parseInt(h, 16), 255)))}
+
+            var r = 0, g = 0, b = 0;
+            v = v || '#fff';
+            if(v.match(/^#[0-9a-f]{3}$/i)) {
+                r = hex2dec(v.substr(1, 1) + v.substr(1, 1)), g = hex2dec(v.substr(2, 1) + v.substr(2, 1)), b = hex2dec(v.substr(3, 1) + v.substr(3, 1))
+            } else if(v.match(/^#[0-9a-f]{6}$/i)) {
+                r = hex2dec(v.substr(1, 2)), g = hex2dec(v.substr(3, 2)), b = hex2dec(v.substr(5, 2))
+            }
+            return r + ',' + g + ',' + b
+        },
         G: function(v) {return(document.getElementById(v));},
         R: function(v) {return(document.body.removeChild(v));},
         A: function(v) {return(document.body.appendChild(v));},
         C: function(v) {return(document.createElement(v));},
-        extend: function(subClass, superClass) {
+        extend: function() {
+            var destination = arguments[0] || {},
+                source = arguments[1] || {};
+//            console.log('before all:');
+//            console.log(destination);
+            for(var property in source) {
+//                console.log('before operation :');
+//                console.log(destination);
+//                console.log('the attribute:' + property);
+                destination[property] = source[property];
+//                console.log('after operation :');
+//                console.log(destination);
+            }
+            return destination;
+        },
+        extends: function(subClass, superClass) {
             //见到网上有人是这样写的
             //subClass.prototype=superClass.prototype;
             //subClass.prototype.constructor=subClass;
